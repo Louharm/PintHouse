91ab5bbde02f3b3c738d0ba39dd14ba81d7edd85
louharm <etu39951@henallux.be>
2020-12-29 11:39:09 +0100

    add translation option and change detail for category

--- pinthouse/src/main/java/com/spring/henallux/pinthouse/controller/SuperController.java
+++ pinthouse/src/main/java/com/spring/henallux/pinthouse/controller/SuperController.java
@@ -41,8 +41,4 @@ public class SuperController {
         model.addAttribute("title","Pinthouse");
         return "integrated:contact";
     }
-    @RequestMapping (value="/category", method = RequestMethod.POST)
-    public String category (Model model){
-        return "integrated:category";
-    }
 }
